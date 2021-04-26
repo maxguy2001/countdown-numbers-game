75435d9d465b55945c64a9eb358a71961bf27eea
maxguy2001 <max.guy2001@gmail.com>
2021-04-26 03:53:48 +0100

    possible part of solution

--- countdown solver.py
+++ countdown solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 def choose_nums(big, small):
     big_nums = [100, 75, 50, 25]
@@ -12,6 +13,32 @@ def choose_nums(big, small):
 
     return numbers
 
-def solve(numbers):
+def operation_orders(iterations):
+    """
+    This function returns the order of the 5 operations taken between the 6 numbers to get the target number.
+    Each integer corresponds to a basic algabraic operation as such:
+    0-add
+    1-subtract
+    2-multiply
+    3-divide
+    """
+    ops = [0, 1, 2, 3]
+    opord = list(itertools.product(ops, ops, ops, ops, ops))
+    opord_inst = opord[iterations]
+
+    return opord_inst
+
+def solve(numbers, target):
+    all_combinations = list(itertools.permutations(numbers))
+#    possible_iterations = np.math.factorial(len(numbers)) * 4**(len(numbers) - 1)
+    i = 0
+    while i < len(all_combinations):
+        combination = list(all_combinations[i])
+
+
+
+
+    return True
+
 
 
