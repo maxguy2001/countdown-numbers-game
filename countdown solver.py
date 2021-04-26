529bfd5a6be24598e334dafcc14a0fa3b8412909
maxguy2001 <max.guy2001@gmail.com>
2021-04-26 04:27:35 +0100

    partial solution

--- countdown solver.py
+++ countdown solver.py
@@ -1,5 +1,6 @@
 import numpy as np
-import itertools
+from itertools import product
+from itertools import permutations
 
 def choose_nums(big, small):
     big_nums = [100, 75, 50, 25]
@@ -23,22 +24,60 @@ def operation_orders(iterations):
     3-divide
     """
     ops = [0, 1, 2, 3]
-    opord = list(itertools.product(ops, ops, ops, ops, ops))
+    opord = list(product(ops, ops, ops, ops, ops))
     opord_inst = opord[iterations]
 
     return opord_inst
 
 def solve(numbers, target):
-    all_combinations = list(itertools.permutations(numbers))
+    all_combinations = list(permutations(numbers))
 #    possible_iterations = np.math.factorial(len(numbers)) * 4**(len(numbers) - 1)
-    i = 0
-    while i < len(all_combinations):
-        combination = list(all_combinations[i])
+    operands = numbers
+    operators = ['+', '*', '-', '//']  # change '//' to '/' for floating point division
+    for opers in product(operators, repeat=len(operands) - 1):
+        formula = [str(operands[0])]
+        for op, operand in zip(opers, operands[1:]):
+            formula.extend([op, str(operand)])
+        formula = ' '.join(formula)
+    ##    print('{} = {}'.format(formula, eval(formula)))
+        if eval(formula) == target:
+            return formula
+        else:
+            return "No solution found"
 
+def target_number():
+    return np.random.randint(100, 999)
 
+def main():
+    chs_or_rand = str(input("Would you like to choose numbers or take random ones? \n (type choose or random)"))
+    if chs_or_rand == "choose":
+        numbers = []
+        for i in range(6):
+            x = int(input("Input number:"))
+            numbers.append(x)
+        target = target_number()
+        print(f"Your numbers are: {numbers}. \n The target number is {target}")
+        print("When you are ready to get solutions, enter y")
+        cont = str(input())
+        if cont == "y":
+            print(f"A possible solution is : {solve(numbers, target)}")
+        else:
+            True
 
+    elif chs_or_rand == "random":
+        big = int(input("How many large numbers would you like?"))
+        small = 6 - big
+        numbers = choose_nums(big, small)
+        target = target_number()
+        print(f"Your numbers are: {numbers}. \n The target number is {target}")
+        print("When you are ready to get solutions, enter y")
+        cont = str(input())
+        if cont == "y":
+            print(f"A possible solution is : {solve(numbers, target)}")
+        else:
+            True
 
-    return True
+main()
 
 
 
